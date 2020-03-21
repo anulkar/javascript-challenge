ccf8cdba7e267b32308267c920b34d52f597fcfa
Atul Nulkar <nulkar@gmail.com>
2020-03-20 23:39:18 -0400

    Added code to append new rows of data for each UFO sighting

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,28 @@
-// from data.js
+// Initialize tableData from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+/* 
+1. Use d3 to append one table row `tr` for each ufoSightings object
+2. Use `Object.entries` to append a cell to the table row for each value in the ufoSighting object 
+3. Use d3 to update each cell's text with ufoSightings values (date, city, state, country, shape, duration and comments)
+*/
+tableData.forEach((ufoSightings) => 
+  {
+    var row = tbody.append("tr");
+    Object.entries(ufoSightings).forEach(([key, value]) => 
+      {
+        var cell = row.append("td");
+        if (key == "state" || key == "country")
+          {
+           // Convert state and country abbreviations to upper case
+           cell.text(value.toUpperCase());
+          }
+        else
+          {
+           cell.text(value);
+          }
+      });
+  }); 
