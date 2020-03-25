ac0b43868ca63dd3702953115f25cdd68fac15a9
Atul Nulkar <nulkar@gmail.com>
2020-03-25 00:06:47 -0400

    Added populateDropdowns function to initialize filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -38,16 +38,57 @@ function displayUFOSightings(ufoSightings)
       });
   }
 
+/* 
+Function to populate the State, Country and Shape dropdown filters
+*/
+function populateDropdowns(ufoSightings)
+{
+  // Get a reference to the state, country and shape dropdown lists
+  var stateFilter = d3.select("#state-filter");
+  var countryFilter = d3.select("#country-filter");
+  var shapeFilter = d3.select("#shape-filter");
+
+  var states = ufoSightings.map(function(ufo) {
+    return ufo.state.toUpperCase();
+  });
+  states = Array.from(new Set(states)).sort();
+  stateFilter.selectAll("option").data(states).enter().append("option").text(function(state) {return state;});
+
+  var countries = ufoSightings.map(function(ufo) {
+    return ufo.country.toUpperCase();
+  });
+  countries = Array.from(new Set(countries)).sort();
+  countryFilter.selectAll("option").data(countries).enter().append("option").text(function(country) {return country;});
+
+  var shapes = ufoSightings.map(function(ufo) {
+    return ufo.shape;
+  });
+  shapes = Array.from(new Set(shapes)).sort();
+  shapeFilter.selectAll("option").data(shapes).enter().append("option").text(function(shape) {return shape;});
+
+  
+}
+
 /*
 1. Listen for the `Filter Table` button click event
 2. Search through the date/time column of the ufoSightings object to find rows that match user input.
 */
 filterTableButton.on("click", function(){
-  // Select the datetime form-control input element
+  // Select all the form-control input elements
   var dateInputElement = d3.select("#datetime");
+  var cityInputElement = d3.select("#city");
+  var stateInputElement = d3.select("#state");
+  var countryInputElement = d3.select("#country");
+  var shapeInputElement = d3.select("#shape");
 
-  // Get the value property of the datetime form-control element
+  // Get the value property of all the form-control elements
   var dateInputValue = dateInputElement.property("value");
+  var cityInputValue = cityInputElement.property("value");
+  var stateInputValue = stateInputElement.property("value");
+  var countryInputValue = countryInputElement.property("value");
+  var shapeInputValue = shapeInputElement.property("value");
+  
+  console.log(dateInputValue + "," + cityInputValue + "," + stateInputValue + "," + countryInputValue + "," + shapeInputValue)
 
   // Filter the ufoSightings object based on the datetime user input
   var filteredUFOSightings = ufoSightings.filter(ufo => ufo.datetime === dateInputValue);
@@ -58,3 +99,4 @@ filterTableButton.on("click", function(){
 
 // Display all available UFO Sightings data by default
 displayUFOSightings(ufoSightings);
+populateDropdowns(ufoSightings);
